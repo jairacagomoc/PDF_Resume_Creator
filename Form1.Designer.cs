a3c72551b899c5423fe865152f615daef28ac57c
jairacagomoc <jhairabcagomoc@gmail.com>
2022-02-27 23:11:12 +0800

    read json file

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,49 +30,53 @@ namespace PDF_Resume_Creator
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.listView1 = new System.Windows.Forms.ListView();
             this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(12, 2);
+            this.button1.Location = new System.Drawing.Point(14, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(102, 33);
+            this.button1.Size = new System.Drawing.Size(96, 31);
             this.button1.TabIndex = 0;
             this.button1.Text = "Load JSON";
             this.button1.UseVisualStyleBackColor = true;
-            // 
-            // listView1
-            // 
-            this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(12, 41);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(302, 386);
-            this.listView1.TabIndex = 1;
-            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.button1.Click += new System.EventHandler(this.btnLoad_Click);
             // 
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(199, 2);
+            this.button2.Location = new System.Drawing.Point(212, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(115, 33);
+            this.button2.Size = new System.Drawing.Size(102, 31);
             this.button2.TabIndex = 2;
             this.button2.Text = "Generate PDF";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(14, 41);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(300, 394);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(329, 447);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.listView1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "  ";
+            this.Text = "PDF Resume Creator";
             this.ResumeLayout(false);
 
         }
@@ -80,8 +84,8 @@ namespace PDF_Resume_Creator
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,96 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace PDF_Resume_Creator
 {
     public partial class Form1 : Form
     {
+        private readonly string _path = $"C:\\Documents\\5th 1st Sem\\Object-Oriented Programming\\PDF_Resume_Creator\\PDF_Resume_Creator\\Resume.json;";
+        
         public Form1()
         {
             InitializeComponent();
+           
+        }
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string jsonFromFile;
+                using (var reader = new StreamReader(_path))
+                {
+                    jsonFromFile = reader.ReadToEnd();
+                }
+                richTextBox1.Text = jsonFromFile;
+
+                var resumeFromJson = JsonConvert.DeserializeObject< Resume >(jsonFromFile);
+            }
+            catch(Exception ex)
+            {
+                //ignored
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
+    public class Resume
+    {
+        public List<BASIC_INFORMATION> BASIC_INFORMATION { get; set; }
+        public List <EDUCATIONAL_BACKGROUND> EDUCATIONAL_BACKGROUND { get; set; }
+        public List <SKILLS> SKILLS { get; set; }
+    }
+    public class BASIC_INFORMATION
+    {
+        public Name Name { get; set; }
+        public Address Address { get; set; }
+        public Birthday Birthday { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public string Nationality { get; set; }
+    }
+    public class Name
+    {
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+    }
+    public class Address
+    {
+        public string FirstName { get; set; }
+    }
+    public class Birthday
+    {
+        public string FirstName { get; set; }
+    }
+    public class EDUCATIONAL_BACKGROUND
+    {
+    public List <Tertiary> Tertiary { get; set; }
+    public List <Secondary> Secondary { get; set; }
+}
+    public class Tertiary
+    {
+        public string School { get; set; }
+        public int Year_Graduated { get; set; }
+    }
+    public class Secondary
+    {
+        public string School { get; set; }
+        public int Year_Graduated { get; set; }
+    }
+    public class SKILLS
+{
+    public string Computer_Literate { get; set; }
+    public string Others { get; set; }
+}
+
 }
